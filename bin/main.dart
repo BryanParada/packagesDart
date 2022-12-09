1722712361ad7debe5f02dbf2261e2fd8e5240a6
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-09 14:09:49 -0300

    D113 - Http countries

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,14 @@
 //import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-
+ 
 import 'package:packages_dart/packages_dart.dart' as packages_dart;
-
-import 'package:packages_dart/classes/reqres_response.dart';
+ 
 
 void main(List<String> arguments) {
 
-  final url = Uri.parse('https://reqres.in/api/users?page=2');
-  http.get(url).then( (resp) {
-
-    final resReqRes = reqResResponseFromJson( resp.body);
-
-
-    //print(resp);
-
-    // final body = jsonDecode(resp.body);
-    // print(body);
-    // print('page: ${body['page']}');
-    // print('per_page: ${body['per_page']}');
-    // print('id third element: ${body['data'][2]}');
-    // print('id third element: ${body['data'][2]['id']}');
-    
-    print('page: ${resReqRes.page}');
-    print('per_page: ${resReqRes.perPage}');
-    print('id third element: ${resReqRes.data[2]}');
-    print('id third element: ${resReqRes.data[2].id}');
-
-
-  });
  
+  //packages_dart.getReqResp_service();
+  packages_dart.getPais();
 
    
 }
