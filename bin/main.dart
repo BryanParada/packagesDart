a898145103544a24377e7e36d9915286a85bb941
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-09 11:49:04 -0300

    D105 - Initial Project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:packages_dart/packages_dart.dart' as packages_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${packages_dart.calculate()}!');
+}
---
+++ test/packages_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:packages_dart/packages_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
